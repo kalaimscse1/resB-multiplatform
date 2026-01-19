33fec67d9016e13d5d7471c4e943480a9a633bc2
Kalaivanan-1996 <kalaimscse1@gmail.com>
2026-01-19 14:46:23 +0530

    staff issue fixed

--- app/src/main/java/com/warriortech/resb/screens/settings/StaffSettingsScreen.kt
+++ app/src/main/java/com/warriortech/resb/screens/settings/StaffSettingsScreen.kt
@@ -4,6 +4,8 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -15,6 +17,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
